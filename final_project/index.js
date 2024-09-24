e46fdee1ad9b122729b21a9b9c6cc3adbdaf7077
kindirsurprise <127309377+kindirsurprise@users.noreply.github.com>
2024-09-24 08:36:32 +0400

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -18,7 +18,7 @@ app.use("/customer/auth/*", function auth(req,res,next){
     }
 
     // Verify the token
-    jwt.verify(token, "seckey", (err, decoded) => {
+   jwt.verify(token.split(' ')[1], "your_secret_key", (err, decoded) => {
         if (err) {
             return res.status(401).json({ message: "Invalid token" });
         }
